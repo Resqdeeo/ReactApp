98e56154f0450fba9e288c50262ee9ff8afd6c4d
Resqdeeo <vlazer03@mail.ru>
2024-02-17 11:22:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [student, setStudent] = useState("Лазарев Валерий");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img width="100px" src="https://sun9-63.userapi.com/impg/AFnc1NtJysf-lu-rrI0pT28AqpKtPQ4Ye2_ItQ/nDQoss_3CKA.jpg?size=1275x1621&quality=95&sign=81a79fac5be6ea0ca2c820b58728100a&type=album" />
+      <p><b>Студент:</b> {student} </p>
+      <Balls />
+    </>
   );
+}
+
+function Balls() {
+
+  const [balls, setBalls] = useState(0);
+
+  const handleAddButton = () => {
+    setBalls(balls + 20);
+  }
+
+  return (
+    <>
+      <p><b>Баллы:</b> {balls}</p>
+      <button onClick={handleAddButton}>Прибавить баллы</button>
+    </>
+  );
+}
+
+function StudentCard() {
+
 }
 
 export default App;
